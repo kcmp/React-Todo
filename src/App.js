4391cfd2da9674fc7998c6aae4326b6ba7d739fa
unknown <kcmp711@gmail.com>
2019-03-07 19:30:59 -0500

    started styling in Todo.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-
 import TodoList from "./components/TodoComponents/TodoList";
 import TodoForm from "./components/TodoComponents/TodoForm";
+// import "./Todo.css";
 
 const currentList = [
   {
