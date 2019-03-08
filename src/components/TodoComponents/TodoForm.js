8920fe2769de7ae23257d88bdfafeeddb3e45b60
unknown <kcmp711@gmail.com>
2019-03-07 21:01:52 -0500

    add for styling to Todo.css

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,19 +3,21 @@ import "./Todo.css";
 
 const TodoForm = props => {
   return (
-    <form onSubmit={props.addItemHandler}>
-      <input
-        type="text"
-        name="inputValue"
-        placeholder="Add new item to the list here"
-        value={props.inputValue}
-        onChange={props.handleChange}
-      />
-      <button className="submit">Add to List</button>
-      <button className="remove" onClick={props.removeCompleted}>
-        Remove Completed Tasks
-      </button>
-    </form>
+    <div className="form">
+      <form onSubmit={props.addItemHandler}>
+        <input
+          type="text"
+          name="inputValue"
+          placeholder="Add new item to the list here"
+          value={props.inputValue}
+          onChange={props.handleChange}
+        />
+        <button className="submit">Add to List</button>
+        <button className="remove" onClick={props.removeCompleted}>
+          Remove Completed Tasks
+        </button>
+      </form>
+    </div>
   );
 };
 
